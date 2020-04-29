06e2239ec6b458aa402ffc1e6620489ad2979326
Brandon <bfont@Brandon.local>
2020-04-29 13:09:53 -0400

    runStream.sh update

--- Stream/runStream.sh
+++ Stream/runStream.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 gcc -O stream.c -o stream
-for i in `seq 20`; do ./stream; done
+mkdir -p results
+log="results/docker.log"
+now=`date`
+echo "Running Stream Benchmark. Started at $now"
+echo "-----------------------------------------------------------------"
+echo "Running Stream Benchmark. Started at $now" >> $log
+for i in `seq 20`; do ./stream >> $log; done
+echo -n "Stream Benchmark completed at "; date
